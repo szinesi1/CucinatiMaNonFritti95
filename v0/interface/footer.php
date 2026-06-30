cd61db89e128e29f98fb972d1e96dcb80990ee68
gaiarota51 <168990433+gaiarota51@users.noreply.github.com>
2026-06-30 18:50:43 +0200

    Update footer.php

--- v0/interface/footer.php
+++ v0/interface/footer.php
@@ -2,7 +2,9 @@
 </div> <!-- chiude layout -->
 
 <footer class="footer">
-    <p>© 2026 — Progetto d'esame Programmazione Web</p>
+    <p>&copy; 2026 CucinatiMaNonFritti95</p>
+    <p>Tutte le ricette sono a scopo dimostrativo.</p>
+    <p>Progetto del corso di Programmazione Web - A.A. 2025/2026</p>
 </footer>
 
 </body>
